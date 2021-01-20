c6b940b532d6613f5d1d2b94df9c2c0f7ff21302
Ujjwal Singh <singhujjwal@gmail.com>
2021-01-20 13:39:06 +0000

    .

--- movie-service/app/main.py
+++ movie-service/app/main.py
@@ -6,7 +6,7 @@ from app.api.db import metadata, database, engine
 
 metadata.create_all(engine)
 
-app = FastAPI(openapi_url="/api/v1/movies/openapi.json", docs_url="/api/v1/movies/docs"))
+app = FastAPI(openapi_url="/api/v1/movies/openapi.json", docs_url="/api/v1/movies/docs")
 
 @app.on_event("startup")
 async def startup():
@@ -15,8 +15,5 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
-
-
-#app.include_router(movies)
 
 app.include_router(movies, prefix='/api/v1/movies', tags=['movies'])
