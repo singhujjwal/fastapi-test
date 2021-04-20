151e5972275be0c67b9233e033d47346f161ed01
Ujjwal Singh <ujjwal.singh@hallibnurton.com>
2021-04-20 19:01:22 +0530

    url_Svc

--- url_service/app/api/url_manager.py
+++ url_service/app/api/url_manager.py
@@ -3,8 +3,12 @@ from app.api.models import UrlIn, UrlOut
 
 async def get_short_url(payload: UrlIn):
     print ("Getting short url....")
-    return "short_url"
+    result_json = {}
+    result_json['short_url'] = 'my_short_url'
+    return result_json
 
 async def get_long_url(short_url: str):
     print ("Getting Long url....")
-    return "long_url"
+    result_json = {}
+    result_json['long_url'] = 'my_long_url'
+    return result_json
