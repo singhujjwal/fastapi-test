cbdbf6ccf350ffa7825fd35be136b941ac6cd71e
Fox <singhujjwal@gmail.com>
2022-01-29 23:41:36 +0530

    updated running commands

---
+++ healthz/README.md
@@ -0,0 +1,5 @@
+## A basic healthz server to test the LBs
+
+sudo yum update -y
+sudo yum groupinstall "Development Tools" -y
+sudo yum install openssl-devel libffi-devel bzip2-devel wget -y
--- healthz/run-server.sh
+++ healthz/run-server.sh
@@ -1,1 +1,6 @@
-uvicorn app.main:app --reload --host 0.0.0.0 --port 8121
+uvicorn app.main:app --reload --host 0.0.0.0 --port 8121 &
+uvicorn app.main:app --reload --host 0.0.0.0 --port 8122 &
+uvicorn app.main:app --reload --host 0.0.0.0 --port 8123 &
+uvicorn app.main:app --reload --host 0.0.0.0 --port 8124 &
+
+## shutdown pkill uvicorn
